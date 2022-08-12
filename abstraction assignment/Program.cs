6ce75615fa3bb6c025c73498c6e16dad0a73ceda
A-Olawumi <adedirefausat@gmail.com>
2022-08-12 08:22:07 +0100

    Add project files.

---
+++ abstraction assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace abstraction_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
